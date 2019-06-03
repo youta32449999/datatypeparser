8664be522cea6f72acf286e89b027621d52c1264
syo <M.youta.kwg@gmail.com>
2019-06-04 00:34:09 +0900

    データ型算出方法全見直し

--- src/main/scala/DataTypeCalculator.scala
+++ src/main/scala/DataTypeCalculator.scala
@@ -4,66 +4,120 @@ import scala.annotation.tailrec
 
 object DataTypeCalculator {
 
-  // data type info from parse result
-  private[this] case class Data(name: String, component: Seq[DataTypeSize], arrayLength: Int, isStruct: Boolean) {
-    // データ型のサイズに０がある場合は常に０を返す
-    def size: Int = {
-      val sum = component.foldLeft(0){(acc, e) => e.size + acc} * arrayLength
-      val resultBuff = if(sum % toCalForm(1) == 0) sum else toCalForm((sum / toCalForm(1))) + toCalForm(1)
-      val result = if(isStruct && resultBuff % toCalForm(2) != 0) resultBuff + toCalForm(1) else resultBuff
-      val containsZero = component.filter(e => e.size == 0) != Nil
-      if (containsZero) 0 else result
-    }
-  }
-
-  private[this] case class DataTypeSize(name: String, size: Int)
-
+  private[this] case class DataTypeSize(name: String, bitSize: Int, wordSize: Int)
 
   private[this] val primitiveDataTypeSize = Seq(
-    DataTypeSize("BOOL", toCalForm("0.0625")),
-    DataTypeSize("WORD", toCalForm("1")),
-    DataTypeSize("INT", toCalForm("1")),
-    DataTypeSize("UINT", toCalForm("1")),
-    DataTypeSize("DWORD", toCalForm("2")),
-    DataTypeSize("DINT", toCalForm("2")),
-    DataTypeSize("UDINT", toCalForm("2")),
-    DataTypeSize("REAL", toCalForm("2")),
-    DataTypeSize("TIME", toCalForm("2"))
+    DataTypeSize("BOOL", 1, 1),
+    DataTypeSize("WORD", 16, 1),
+    DataTypeSize("INT", 16, 1),
+    DataTypeSize("UINT", 16, 1),
+    DataTypeSize("DWORD", 32, 2),
+    DataTypeSize("DINT", 32, 2),
+    DataTypeSize("UDINT", 32, 2),
+    DataTypeSize("REAL", 32, 2),
+    DataTypeSize("TIME", 32, 2)
   )
 
+  // data type info from parse result
+  private[this] sealed trait Data {
 
-  // 計算用に小数点以下桁上げ
-  private[this] def toCalForm(datasize: String): Int = (datasize.toFloat * 10000).toInt
+    /**
+      * 1ワードのビット数を返す
+      * @return
+      */
+    def word: Int = 16
+
+    /**
+      * TypeAlias 1ビット単位
+      * Array 1ワード単位に端数切り上げ
+      * Struct 2ワード単位に端数切り上げ
+      * @return データ型のビット単位のサイズ
+      */
+    def bitSize :Int
+
+    /**
+      * TypeAlias 1ワード単位に切り上げ
+      * Array bitSizeと同様
+      * Struct bitSizeと同様
+      * @return データ型のワード単位のサイズ
+      */
+    def wordSize: Int
+  }
+  private[this] case class AliasData(name: String, component: DataTypeSize) extends Data {
+
+    override def bitSize: Int = component.bitSize
+
+    override def wordSize: Int = component.wordSize
+
+  }
+  private[this] case class ArrayData(name: String, component: DataTypeSize, firstIndex: Int, lastIndex: Int) extends Data {
+
+    override def bitSize: Int = {
+      val elementNum = (lastIndex - firstIndex) + 1
+      val bitNum = component.bitSize * elementNum
+      val wordNum = (bitNum / super.word) + (if (bitNum % super.word == 0) 0 else 1)
+      wordNum * super.word
+    }
+
+    override def wordSize: Int = bitSize / super.word
+  }
+  private[this] case class StructData(name: String, component: Seq[DataTypeSize]) extends Data {
+
+    override def bitSize: Int = {
+      val bitNum = component.foldLeft(0){(acc, e) => acc + e.bitSize}
+      val wordNumBuff = bitNum / (super.word)
+      val wordNum = wordNumBuff + (if(bitNum % (super.word * 2) == 0) 0 else (if (wordNumBuff % 2 == 0) 2 else 1))
+      val isContainsZeroSize = component.filter(e => e.bitSize == 0).length > 0
+      if(isContainsZeroSize) 0 else wordNum * super.word
+    }
+
+    override def wordSize: Int = bitSize / super.word
+  }
 
 
-  private[this] def toCalForm(datasize: Int): Int = (datasize * 10000)
-
-
-  //　計算用から実際のサイズに戻す
-  private[this] def fromCalForm(datasize: Int): Int = datasize / 10000
-
-
-  // parse result convert for DataType
+  /**
+    * Converts parse result for DataType
+    * @param seq parse result
+    * @return
+    */
   private[this] def convertToData(seq: Seq[DataType]): Seq[Data] = seq.map(e => e match {
-    case TypeAlias(name, datatype) => Data(name, Seq(DataTypeSize(datatype, 0)), 1, false)
-    case TypeArray(name, firstIndex, lastIndex, datatype) => Data(name, Seq(DataTypeSize(datatype, 0)), lastIndex - firstIndex + 1, false)
-    case TypeStruct(name, member) => Data(name, convertToData(member).map(e => e.component).flatten, 1, true)
+    case TypeAlias(name, datatype) => AliasData(name, DataTypeSize(datatype, 0, 0))
+    case TypeArray(name, firstIndex, lastIndex, datatype) => ArrayData(name, DataTypeSize(datatype, 0, 0), firstIndex, lastIndex)
+    case TypeStruct(name, member) => StructData(name, convertToData(member).map{ //構造体のメンバは型エイリアスのみなので再帰は1回のみ
+      d => d match {
+      case data@AliasData(_, _) => DataTypeSize(data.component.name, 0, 0)
+      case data@ArrayData(_, _, _, _) => DataTypeSize(data.name, 0, 0)
+      case data@StructData(_, _) => DataTypeSize(data.name, 0, 0)
+    }})
   })
 
+  /**
+    * Calculates data type size
+    * @param parseResultSeq
+    * @return
+    */
   private[this] def calculate(parseResultSeq: Seq[DataType]): Seq[Data] = {
     @tailrec
     def loop(sizeSeq: Seq[DataTypeSize], dataSeq: Seq[Data]): Seq[Data] = sizeSeq match {
       case x::xs => {
         // update data type size in component
-        val newDataSeq = dataSeq.map{e =>
-          val newComponent = e.component.map{c => if(c.name == x.name) c.copy(size = x.size) else c}
-          e.copy(component = newComponent)
-        }
+        val newDataSeq: Seq[Data] = dataSeq.map{e => e match {
+          case data@AliasData(_, _) => if(data.component.name == x.name) data.copy(component = data.component.copy(bitSize = x.bitSize, wordSize = x.wordSize)) else data
+          case data@ArrayData(_, _, _, _) => if(data.component.name == x.name) data.copy(component = data.component.copy(bitSize = x.bitSize, wordSize = x.wordSize)) else data
+          case data@StructData(_, _) => {
+            val newComponent = data.component.map(c => if(c.name == x.name) c.copy(bitSize = x.bitSize, wordSize = x.wordSize) else c)
+            data.copy(component = newComponent)
+          }
+        }}
 
         // サイズが決まったデータ型を追加する。無ければ追加なし
         val dicidedSizeList = newDataSeq.zip(dataSeq)
-          .filter{case (newSeqElem, oldSeqElem) => newSeqElem.size != 0 && oldSeqElem.size == 0}
-          .map{case (n,_) => DataTypeSize(n.name, n.size)}
+          .filter{case (newSeqElem, oldSeqElem) => newSeqElem.bitSize != 0 && oldSeqElem.bitSize == 0}
+          .map{case (n,_) => n match {
+            case data@AliasData(_, _) => DataTypeSize(data.name, data.bitSize, data.wordSize)
+            case data@ArrayData(_, _, _, _) => DataTypeSize(data.name, data.bitSize, data.wordSize)
+            case data@StructData(_, _) => DataTypeSize(data.name, data.bitSize, data.wordSize)
+          }}
         val newSizeSeq = xs ++ dicidedSizeList
         loop(newSizeSeq, newDataSeq)
       }
@@ -75,6 +129,10 @@ object DataTypeCalculator {
   }
 
   def apply(parseResultSeq: Seq[DataType]): Map[String, Int] = calculate(parseResultSeq)
-    .map(e => e.name -> fromCalForm(e.size))
+    .map(e => e match {
+      case data@AliasData(_, _) => data.name -> data.wordSize
+      case data@ArrayData(_, _, _, _) => data.name -> data.wordSize
+      case data@StructData(_, _) => data.name -> data.wordSize
+    })
     .toMap
 }
