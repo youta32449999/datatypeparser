ade83ff195003a9a2599d1ef4138857cfb96ab2e
syo <M.youta.kwg@gmail.com>
2019-06-04 00:32:41 +0900

    modify:テストケースの修正

--- src/test/scala/DataTypeCalculatorSpec.scala
+++ src/test/scala/DataTypeCalculatorSpec.scala
@@ -39,20 +39,24 @@ class DataTypeCalculatorSpec extends FlatSpec with DiagrammedAssertions {
 
 
     val boolList00_15 = (0 to 15).map(e => TypeAlias("BOOL_ALIAS" + e.toString, "BOOL")).toList
-    val boolList00_31 = (0 to 15).map(e => TypeAlias("BOOL_ALIAS" + e.toString, "BOOL")).toList
-    val intList1_3 = (1 to 3).map(e => TypeAlias("INT_ALIAS" + e.toString, "INT")).toList
+    val boolList00_31 = (0 to 31).map(e => TypeAlias("BOOL_ALIAS" + e.toString, "BOOL")).toList
+    val intList1_4 = (1 to 4).map(e => TypeAlias("INT_ALIAS" + e.toString, "INT")).toList
     val dintList1_3 = (1 to 3).map(e => TypeAlias("DINT_ALIAS" + e.toString, "DINT")).toList
     val typeStructList: Seq[DataType] = Seq(
       TypeStruct("BOOL_STRUCT1", Seq(TypeAlias("BOOL_ALIAS", "BOOL"))),
       TypeStruct("BOOL_STRUCT2", boolList00_15),
       TypeStruct("BOOL_STRUCT3", boolList00_31),
       TypeStruct("BOOL_STRUCT4", boolList00_31 :+ TypeAlias("BOOL_ALIAS32", "BOOL")),
-      TypeStruct("INT_STRUCT1", intList1_3),
-      TypeStruct("INT_STRUCT2", intList1_3 :+ TypeAlias("INT_ALIAS4", "INT")),
+      TypeStruct("INT_STRUCT1", intList1_4),
+      TypeStruct("INT_STRUCT2", intList1_4 :+ TypeAlias("INT_ALIAS5", "INT")),
       TypeStruct("DINT_STRUCT1", dintList1_3),
       TypeStruct("DINT_STRUCT2", dintList1_3 :+ TypeAlias("DINT_ALIAS", "DINT")),
-      TypeStruct("BOOL_ARRAY_STRUCT1", Seq(TypeArray("BOOL_ARRAY1", 0, 31, "BOOL"))),
-      TypeStruct("BOOL_ARRAY_STRUCT2", Seq(TypeArray("BOOL_ARRAY2", 0, 7, "BOOL"), TypeArray("BOOL_ARRAY3", 0, 7, "BOOL"), TypeArray("BOOL_ARRAY4", 0, 10, "BOOL")))
+      TypeArray("BOOL_ARRAY1", 0, 31, "BOOL"),
+      TypeArray("BOOL_ARRAY2", 0, 7, "BOOL"),
+      TypeArray("BOOL_ARRAY3", 0, 7, "BOOL"),
+      TypeArray("BOOL_ARRAY4", 0, 7, "BOOL"),
+      TypeStruct("BOOL_ARRAY_STRUCT1", Seq(TypeAlias("BOOL_ARRAY_1", "BOOL_ARRAY1"))),
+      TypeStruct("BOOL_ARRAY_STRUCT2", Seq(TypeAlias("BOOL_ARRAY_2", "BOOL_ARRAY2"), TypeAlias("BOOL_ARRAY_3", "BOOL_ARRAY3"), TypeAlias("BOOL_ARRAY_4", "BOOL_ARRAY4")))
       )
 
     assert(DataTypeCalculator(typeStructList) == Map(
@@ -64,6 +68,10 @@ class DataTypeCalculatorSpec extends FlatSpec with DiagrammedAssertions {
       "INT_STRUCT2" -> 6,
       "DINT_STRUCT1" -> 6,
       "DINT_STRUCT2" -> 8,
+      "BOOL_ARRAY1" -> 2,
+      "BOOL_ARRAY2" -> 1,
+      "BOOL_ARRAY3" -> 1,
+      "BOOL_ARRAY4" -> 1,
       "BOOL_ARRAY_STRUCT1" -> 2,
       "BOOL_ARRAY_STRUCT2" -> 4
     ))
